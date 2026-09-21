6307b40c575a7553e62fd340d1cf6ebdec8ae141
Ali Can Yücel <yucelalican@hotmail.com>
2026-09-21 16:48:09 +0300

    Delete EImza.Domain/Entities directory

--- EImza.Domain/Entities/Permission.cs
+++
@@ -1,9 +0,0 @@
-using EImza.Domain.Abstractions;
-
-namespace EImza.Domain.Entities;
-
-public sealed class Permission : Entity
-{
-    public string Name { get; set; } = string.Empty; // e.g., Documents.View
-    public string Description { get; set; } = string.Empty;
-}
